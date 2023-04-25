2386fef42805d3810bf2b8a013ccb7de490ba536
justhonnoldwegoinghome <wesleytan19@gmail.com>
2023-04-25 19:38:28 +0800

    (fix) add google font
    MUI theme still requires google font link tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
-import { Inter } from "next/font/google";
+import Head from "next/head";
+
 import { Salespersons } from "@/features/salespersons";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return <Salespersons />;
+  return (
+    <>
+      <Head>
+        <title>Realway</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta
+          name="description"
+          content="Find the perfect property agent in Singapore with our app. Use real transaction data and agent expertise to make an informed decision. Say goodbye to biased advertising and hello to reliable recommendations."
+        />
+        <link rel="icon" href="/graphics/hut.svg" />
+      </Head>
+      <Salespersons />
+    </>
+  );
 }
