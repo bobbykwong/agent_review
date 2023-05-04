a90b42bded279f3d907b75474cddafef42f14d49
Bobby Kwong <bobbykwongzw@gmail.com>
2023-05-04 14:50:31 +0800

    Able to Get all salespersons and Filter Salespersons by estateAgentName

--- src/pages/api/salespersons.js
+++ src/pages/api/salespersons.js
@@ -2,11 +2,17 @@ import { mongo } from "@/utils/mongo";
 
 export default async function handler(req, res) {
 
-    const params = req.query
-    res.status(200).json(params);
-  
-    // const db = await mongo.connect();
-    // const salespersons = await db.collection("salespersons").find({}).limit(10).toArray();
+    const db = await mongo.connect();
+    const queryParams = req.query
 
-    // res.status(200).json(salespersons);
-  }
+    // Filter by estate agent
+    if (queryParams["estateAgentName"])
+    {
+      const estateAgentName = queryParams["estateAgentName"]
+      const salespersons = await db.collection("salespersons").find({estateAgentName}).limit(10).toArray()
+      res.status(200).json(salespersons);
+    }
+    const salespersons = await db.collection("salespersons").find({}).limit(10).toArray();
+    res.status(200).json(salespersons);
+
+}
