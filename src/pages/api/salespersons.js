938c86ebaa5b8ba060b51a689d96105876ec5e76
Bobby Kwong <bobbykwongzw@gmail.com>
2023-05-04 14:25:57 +0800

    getting query params. Do not use useRouter to get the params outside of a functional component as it is a react hook that only works within the component

--- src/pages/api/salespersons.js
+++ src/pages/api/salespersons.js
@@ -1,8 +1,12 @@
 import { mongo } from "@/utils/mongo";
 
 export default async function handler(req, res) {
-    const db = await mongo.connect();
-    const salespersons = await db.collection("salespersons").find({}).limit(10).toArray();
 
-    res.status(200).json(salespersons);
+    const params = req.query
+    res.status(200).json(params);
+  
+    // const db = await mongo.connect();
+    // const salespersons = await db.collection("salespersons").find({}).limit(10).toArray();
+
+    // res.status(200).json(salespersons);
   }
