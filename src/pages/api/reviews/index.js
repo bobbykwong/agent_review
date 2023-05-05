cb22fcd4a6b1b231fd8ca28d523ad29ee5b1a75c
Bobby Kwong <bobbykwongzw@gmail.com>
2023-05-05 22:52:46 +0800

    did some hoo haa with the aggregation shit. Just for notes

--- src/pages/api/reviews/index.js
+++ src/pages/api/reviews/index.js
@@ -3,11 +3,17 @@ import { mongo } from "@/utils/mongo";
 export default async function handler(req, res) {
     const db = await mongo.connect();
     const queryParams = req.query;
-
+    
     const salespersonId = queryParams["salespersonId"]
     
+    const filterParams = {}
+    if(salespersonId)
+    {
+        filterParams["salespersonId"] = salespersonId
+    }
+
     const reviews = await db.collection("reviews")
-                                  .find({"salespersonId": salespersonId})
+                                  .find(filterParams)
                                   .toArray();
 
     res.status(200).send(reviews);                                
--- src/pages/api/transactions/index.js
+++ src/pages/api/transactions/index.js
@@ -3,26 +3,41 @@ import { mongo } from "@/utils/mongo";
 export default async function handler(req, res) {
     const db = await mongo.connect();
     const queryParams = req.query
-    const page = parseInt(queryParams["page"]);
-    const salespersonId = queryParams["salespersonId"]
-    let transactions;
+    // const page = parseInt(queryParams["page"]);
+    // const salespersonId = queryParams["salespersonId"]
+    // let transactions;
     
-    // Give all transactions
-    if (!salespersonId)
-    { 
-        transactions = await db.collection("transactions")
-                                  .find({})
-                                  .skip(page)
-                                  .limit(10)
-                                  .toArray();
-    }
-    else{
-        transactions = await db.collection("transactions")
-                                      .find({"salespersonId": salespersonId})
-                                      .skip(page)
-                                      .limit(10)
-                                      .toArray();
-    }
+    // // Give all transactions
+    // if (!salespersonId)
+    // { 
+    //     transactions = await db.collection("transactions")
+    //                               .find({})
+    //                               .skip(page)
+    //                               .limit(10)
+    //                               .toArray();
+    // }
+    // else{
+    //     transactions = await db.collection("transactions")
+    //                                   .find({"salespersonId": salespersonId})
+    //                                   .skip(page)
+    //                                   .limit(10)
+    //                                   .toArray();
+    // }
 
-    res.status(200).send(transactions);                                
+    // res.status(200).send(transactions);    
+    
+    
+    const transactions = await db.collection("transactions")
+                                  .aggregate([
+                                    // {$match: {town: "BUKIT PANJANG"}},
+                                    // {$count: "total transactions"}
+                                    {$group: 
+                                        {
+                                            _id: "$transactionType", 
+                                            count:{$sum:1}
+                                        }
+                                    }                 
+                                  ])    
+                                  .toArray();
+    res.status(200).send(transactions);                                  
 }
