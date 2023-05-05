cb1b7b5903e426261057ec058e78ee001254e33b
Bobby Kwong <bobbykwongzw@gmail.com>
2023-05-05 16:34:57 +0800

    able to get transactions by salesperson

--- src/pages/api/transactions/index.js
+++ src/pages/api/transactions/index.js
@@ -4,12 +4,25 @@ export default async function handler(req, res) {
     const db = await mongo.connect();
     const queryParams = req.query
     const page = parseInt(queryParams["page"]);
+    const salespersonId = queryParams["salespersonId"]
+    let transactions;
     
-    const transactions = await db.collection("transactions")
+    // Give all transactions
+    if (!salespersonId)
+    {
+        transactions = await db.collection("transactions")
                                   .find({})
                                   .skip(page)
                                   .limit(10)
                                   .toArray();
+    }
+    else{
+        transactions = await db.collection("transactions")
+                                      .find({"salespersonId": salespersonId})
+                                      .skip(page)
+                                      .limit(10)
+                                      .toArray();
+    }
 
     res.status(200).send(transactions);                                
 }
