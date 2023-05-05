5aeba422fa9b850712461999707e3dd6b18fadf9
Bobby Kwong <bobbykwongzw@gmail.com>
2023-05-05 13:50:11 +0800

    pagination added

--- src/pages/api/salespersons/index.js
+++ src/pages/api/salespersons/index.js
@@ -4,24 +4,43 @@ export default async function handler(req, res) {
 
     const db = await mongo.connect();
     const queryParams = req.query
-
+    let page = parseInt(queryParams["page"])
+    
     // Filter
     if (Object.keys(queryParams).length > 0)
     {
       // Get the filter key and value from the params
-      // Only able to query one field at a time. Doesn't seem to make sense to query more than one
-      const filterKey = Object.keys(queryParams)[0];
-      const filterValue = Object.values(queryParams)[0];
+      // Only expecting one key other than pagination key. Doesn't seem to make sense to query more than one field.
+      
+      let filterKey;
+      Object.keys(queryParams).forEach(element => {
+        if (element != "page")
+        {
+          filterKey = element
+        }
+      });
+              
+      const filterValue = queryParams[filterKey];
       const filterParams = {}
       filterParams[filterKey] = filterValue
 
       // Query Mongo
-      const salespersons = await db.collection("salespersons").find(filterParams).limit(10).toArray()
+      const salespersons = await db.collection("salespersons")
+                                  .find(filterParams)
+                                  // skip pagination may not be the best way as its performance decreases the further it skips. See range queries
+                                  .skip(page)  
+                                  .limit(10)
+                                  .toArray()
+                                  
       res.status(200).json(salespersons);
     }
     else
     {
-      const salespersons = await db.collection("salespersons").find({}).limit(10).toArray();
+      const salespersons = await db.collection("salespersons")
+                                  .find({})
+                                  .skip(page)
+                                  .limit(10)
+                                  .toArray();
       res.status(200).json(salespersons);
     }
 }
