c81b6fa2a6661e47cb102a9517ac005b6d9c75ed
Bobby Kwong <bobbykwongzw@gmail.com>
2023-05-06 14:23:09 +0800

    added structure for json response in reviews salespersons and transactions able to customise results limit

--- src/pages/api/reviews/index.js
+++ src/pages/api/reviews/index.js
@@ -3,6 +3,8 @@ import { mongo } from "@/utils/mongo";
 export default async function handler(req, res) {
     const db = await mongo.connect();
     const queryParams = req.query;
+    const page = queryParams["page"] ? parseInt(queryParams["page"]) : 0
+    const limit = queryParams["limit"] ? parseInt(queryParams["limit"]) : 10
     
     const salespersonId = queryParams["salespersonId"]
     
@@ -14,7 +16,28 @@ export default async function handler(req, res) {
 
     const reviews = await db.collection("reviews")
                                   .find(filterParams)
+                                  .skip(page)  
+                                  .limit(limit)
                                   .toArray();
-
-    res.status(200).send(reviews);                                
+    
+    const jsonResponse = {
+        "pageToken": page,
+        "totalResults": 1000,
+        "results": reviews
+    }                              
+    res.status(200).json(jsonResponse);    
+    
+    // const reviews = await db.collection("reviews")
+    //                               .aggregate([
+    //                                 // {$match: {town: "BUKIT PANJANG"}},
+    //                                 // {$count: "total transactions"}
+    //                                 {$group: 
+    //                                     {
+    //                                         _id: "$salespersonId", 
+    //                                         count:{$sum:1}
+    //                                     }
+    //                                 }                 
+    //                               ])    
+    //                               .toArray();
+    // res.status(200).send(reviews);
 }
--- src/pages/api/transactions/index.js
+++ src/pages/api/transactions/index.js
@@ -3,41 +3,47 @@ import { mongo } from "@/utils/mongo";
 export default async function handler(req, res) {
     const db = await mongo.connect();
     const queryParams = req.query
-    // const page = parseInt(queryParams["page"]);
-    // const salespersonId = queryParams["salespersonId"]
-    // let transactions;
+    const page = queryParams["page"] ? parseInt(queryParams["page"]) : 0
+    const limit = queryParams["limit"] ? parseInt(queryParams["limit"]) : 10
+    const salespersonId = queryParams["salespersonId"]
+    let transactions;
     
-    // // Give all transactions
-    // if (!salespersonId)
-    // { 
-    //     transactions = await db.collection("transactions")
-    //                               .find({})
-    //                               .skip(page)
-    //                               .limit(10)
-    //                               .toArray();
-    // }
-    // else{
-    //     transactions = await db.collection("transactions")
-    //                                   .find({"salespersonId": salespersonId})
-    //                                   .skip(page)
-    //                                   .limit(10)
-    //                                   .toArray();
-    // }
-
-    // res.status(200).send(transactions);    
-    
-    
-    const transactions = await db.collection("transactions")
-                                  .aggregate([
-                                    // {$match: {town: "BUKIT PANJANG"}},
-                                    // {$count: "total transactions"}
-                                    {$group: 
-                                        {
-                                            _id: "$transactionType", 
-                                            count:{$sum:1}
-                                        }
-                                    }                 
-                                  ])    
+    // Give all transactions
+    if (!salespersonId)
+    { 
+        transactions = await db.collection("transactions")
+                                  .find({})
+                                  .skip(page)
+                                  .limit(limit)
                                   .toArray();
-    res.status(200).send(transactions);                                  
+    }
+    else{
+        transactions = await db.collection("transactions")
+                                      .find({"salespersonId": salespersonId})
+                                      .skip(page)
+                                      .limit(limit)
+                                      .toArray();
+    }
+
+    const jsonResponse = {
+        "pageToken": page,
+        "totalResults": 1000,
+        "results": transactions
+    }
+    res.status(200).json(jsonResponse);    
+    
+    
+    // const transactions = await db.collection("transactions")
+    //                               .aggregate([
+    //                                 {$match: {town: "BUKIT PANJANG"}},
+    //                                 // {$count: "total transactions"}
+    //                                 {$group: 
+    //                                     {
+    //                                         _id: "$salespersonId", 
+    //                                         count:{$sum:1}
+    //                                     }
+    //                                 }                 
+    //                               ])    
+    //                               .toArray();
+    // res.status(200).send(transactions);                                  
 }
