a0bc51771d67ca856b10a97bdab4a62da05bdf10
Bobby Kwong <bobbykwongzw@gmail.com>
2023-05-22 17:25:07 +0800

    Added reviews policy page and updated legal page.

--- src/pages/legal.tsx
+++ src/pages/legal.tsx
@@ -5,6 +5,74 @@ import { PageLayout } from "@/components/layout";
 export default function Page() {
   return (
     <PageLayout>
+      
+      <h1 className="text-4xl font-bold mb-2">Terms and Conditions of Property Agent Review Website</h1>
+
+      Please read these terms and conditions carefully before using our property agent review website. By accessing or using our website, you agree to be bound by these terms and conditions. If you do not agree with any part of these terms and conditions, please refrain from using our website.
+
+      <h2 className="text-2xl font-bold mb-2">Acceptance of Terms</h2>
+      <p className="max-w-screen-desktop text-justify">
+      1.1 By accessing or using our property agent review website (hereinafter referred to as "the Website"), you acknowledge that you have read, understood, and agreed to be bound by these terms and conditions, and any additional guidelines, rules, or policies referenced herein.
+
+      1.2 These terms and conditions may be updated or modified by us at any time without prior notice. Your continued use of the Website after any such changes constitutes your acceptance of the revised terms and conditions.
+      </p>
+
+      <h2 className="text-2xl font-bold mb-2">User Obligations</h2>
+      <p className="max-w-screen-tablet text-justify">
+      2.1 The Website is intended for individuals who are at least 18 years old. By using the Website, you affirm that you are 18 years of age or older.
+
+      2.2 You agree to use the Website only for lawful purposes and in compliance with all applicable laws and regulations. You shall not engage in any activity that may disrupt or interfere with the proper functioning of the Website.
+
+      2.3 You are solely responsible for the accuracy, completeness, and legality of any information or content you submit to the Website.
+
+      2.4 You shall not impersonate any other person or entity or misrepresent your affiliation with any person or entity while using the Website.
+      </p>
+
+      <h2 className="text-2xl font-bold mb-2">Property Agent Reviews</h2>
+      <p className="max-w-screen-tablet text-justify">
+      3.1 The Website provides a platform for users to submit reviews and ratings of property agents.
+
+      3.2 By submitting a review, you represent and warrant that your review is truthful, accurate, and based on your personal experience with the property agent being reviewed.
+
+      3.3 You agree to refrain from submitting any defamatory, fraudulent, or misleading reviews or content that violates any third-party rights, including intellectual property rights.
+
+      3.4 We reserve the right to remove or modify any review or content submitted to the Website at our discretion, without providing any explanation or notification.
+      </p>
+
+      <h2 className="text-2xl font-bold mb-2">Intellectual Property Rights</h2>
+      <p className="max-w-screen-tablet text-justify">
+      4.1 All intellectual property rights, including but not limited to trademarks, logos, and content on the Website, are owned by us or our licensors. You agree not to use, reproduce, distribute, or modify any intellectual property without our prior written consent.
+      </p>
+
+      <h2 className="text-2xl font-bold mb-2">Limitation of Liability</h2>
+      <p className="max-w-screen-tablet text-justify">
+      5.1 The Website and its content are provided on an "as-is" basis without any warranties or representations, either express or implied. We do not guarantee the accuracy, completeness, or reliability of any information or content on the Website.
+
+      5.2 In no event shall we be liable for any indirect, consequential, incidental, punitive, or special damages arising out of or in connection with your use of the Website, including but not limited to lost profits, loss of data, or loss of reputation.
+      </p>
+
+      <h2 className="text-2xl font-bold mb-2">Indemnification</h2>
+      <p className="max-w-screen-tablet text-justify">
+      6.1 You agree to indemnify and hold us harmless from any claims, losses, liabilities, damages, expenses, or costs (including reasonable attorneys' fees) arising out of or in connection with your use of the Website, violation of these terms and conditions, or infringement of any third-party rights.
+      </p>
+
+      <h2 className="text-2xl font-bold mb-2">Termination</h2>
+      <p className="max-w-screen-tablet text-justify">
+      7.1 We reserve the right to terminate or suspend your access to the Website at any time without prior notice or liability, for any reason, including but not limited to your violation of these terms and conditions.
+      </p>
+
+      <h2 className="text-2xl font-bold mb-2">Governing Law</h2>
+      <p className="max-w-screen-tablet text-justify">
+      8.1 These terms and conditions shall be governed by and construed in accordance with the laws of [Jurisdiction]. Any dispute arising out of or in connection with these terms and conditions shall be subject to the exclusive jurisdiction of the courts of [Jurisdiction].
+      </p>
+
+      <h2 className="text-2xl font-bold mb-2">Severability</h2>
+      <p className="max-w-screen-tablet text-justify">
+      9.1 If any provision of these terms and conditions is found to be invalid, illegal, or unenforceable, the remaining provisions shall continue in full force and effect.
+
+      By using our property agent review website, you acknowledge that you have read, understood, and agreed to these terms and conditions.
+      </p>
+
       <h2 className="text-2xl font-bold mb-2">Data source</h2>
       <p className="max-w-screen-tablet text-justify">
         Contains information from{" "}
---
+++ src/pages/reviews-policy/index.tsx
@@ -0,0 +1,75 @@
+import Head from "next/head";
+
+import { HorizontalLayout, PageLayout } from "@/components/layout";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Realway</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta
+          name="description"
+          content="Find the perfect property agent in Singapore with our app. Use real transaction data and agent expertise to make an informed decision. Say goodbye to biased advertising and hello to reliable recommendations."
+        />
+        <link rel="icon" href="/graphics/hut.svg" />
+      </Head>
+      <div className="bg-gray-100">
+        <PageLayout>
+          <div className="py-12">
+            <h1 className="text-5xl font-semibold">
+             Reviews Policy
+            </h1>
+          </div>
+          <div className="py-12">
+              <h2 className="text-3xl font-medium py-4">
+                Who can leave a review?
+              </h2>
+              <p className="mt-2 text-gray-500 tracking-wider text-lg max-w-screen-tablet text-justify">
+                Anyone who has had an interaction with the agent in their capacity as an agent can create an account and leave a review. We think that every part of the experience with an agent is important for helping decide which agent to work with. This also ensures that agents cannot pre-screen who gets to leave a review or moderate the review itself. 
+                <br></br>
+                <br></br>
+                Agents can invite clients to leave a review but are not allowed to provide an incentive or payment for clients to do so.
+                <br></br>
+                <br></br>
+                People who have not had a genuine interaction with the agent in their capacity or service as an agent are not allowed to leave a review. 
+              </p>  
+          </div>
+          <div className="py-12">
+              <h2 className="text-3xl font-medium py-4">
+                How we combat fake reviews
+              </h2>
+              <p className="mt-2 text-gray-500 tracking-wider text-lg max-w-screen-tablet text-justify">
+                When we suspect that a review might be fake, we will ask for proof of interaction from the reviewer via the email provided during sign up. 
+                <br></br>
+                <br></br>
+                Some things that serve as proof of interaction can be but are not limited to transaction records with names, text messages arranging appointments, email correspondence etc. Please be advised that we may request proof of an interaction, and failure to provide these records could lead to the removal of a review.
+              </p>  
+          </div>
+          <div className="py-12">
+              <h2 className="text-3xl font-medium py-4">
+                When is a review taken down?
+              </h2>
+              <p className="mt-2 text-gray-500 tracking-wider text-lg max-w-screen-tablet text-justify">
+                Agents are not allowed to delete reviews, they are only allowed to flag a review if there is doubt on whether the reviewer had an actual interaction with the agent. An agent cannot flag a review simply because a review is negative or unfavourable to them. 
+                <br></br>
+                <br></br>
+                When a review has been flagged, we will investigate the review. If it is found to be a fake review or flout the guidelines for reviews, the review will be taken down.          
+              </p>  
+          </div>
+          <div className="py-12">
+              <h2 className="text-3xl font-medium py-4">
+                General review guidelines
+              </h2>
+              <p className="mt-2 text-gray-500 tracking-wider text-lg max-w-screen-tablet text-justify">
+              Whatever the experience between client and agent, a review should always be respectful to the agent involved. Do not write reviews that are harmful, hateful, discriminatory, defamatory or obscene. 
+              <br></br>
+              <br></br>
+              Do not make up an interaction or write a review for someone else.      
+              </p>  
+          </div>
+        </PageLayout>
+      </div>
+    </>
+  );
+}
