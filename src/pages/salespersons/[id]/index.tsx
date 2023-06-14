75f43a603e4a8ae5c73315951ffa746097d779f3
Bobby Kwong <bobbykwongzw@gmail.com>
2023-06-14 17:09:42 +0800

    open see reviews policy link in new tab to prevent review draft from deleting

--- src/pages/salespersons/[id]/index.tsx
+++ src/pages/salespersons/[id]/index.tsx
@@ -5,15 +5,25 @@ import { SalespersonPage, Salesperson } from "@/features/salespersons";
 import { PageLayout } from "@/components/layout";
 
 export default function Page() {
-  let { query } = useRouter();
-
+  const router = useRouter();
+  
+  // Declaring a variable with curly braces obtains the key of the object.
+  // In this case, router = {"query": {"a" : "b"}, "c" : {"d" : "e"}}
+  // so query = {"a" : "b"}
+  let { query } = router;
+  
   if (!query.id) return <Spinner />;
-
   const id = query.id as Salesperson["id"];
+
+  const handleBackClick = () => {
+    // Handle the back navigation logic here
+    router.back()
+  };
 
   return (
     <div className="bg-gray-100">
       <PageLayout>
+        {/* <button onClick={handleBackClick}>Back</button> */}
         <SalespersonPage id={id} />
       </PageLayout>
     </div>
